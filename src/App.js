7f52aee52b8013f724755ef0f72ea3d2fceeb1ad
nickfff-dev <hunchographics@gmail.com>
2021-12-27 23:45:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Header from './Header';
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid="lg">
+    <Row>
+    <Col sm={true}><Image src={logo} className="App-logo" /> </Col>
+    <Col sm={true}><Header/></Col>
+    <Col sm={true}><img   /> </Col>
+    </Row>
+  </Container>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container';
+import './index.css';
+import './Header.css';
+
+
+
+class Header extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+
+
+
+    render(){
+        return(
+           
+            <Nav className="me-auto justify-content-end">
+              <Nav.Link className="kichwa"  href="#home">Home</Nav.Link>
+              <Nav.Link className="kichwa" href="#features">Work</Nav.Link>
+              <Nav.Link className="kichwa" href="#pricing">About</Nav.Link>
+              <Nav.Link className="kichwa" href="#pricing">Contact</Nav.Link>
+              
+            </Nav>
+            
+        )
+    }
+}
+
+export default Header;
